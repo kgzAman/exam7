681ffa4aeede9f3c1f21b7e447569890058feca2
Aman Kanatbek Uulu <bishkek9408@gmail.com>
2021-05-08 11:29:48 +0600

    #1 initional commit

---
+++ src/test/java/com/aman/exam7/Exam7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aman.exam7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exam7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
